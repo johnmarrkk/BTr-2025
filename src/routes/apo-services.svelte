4f6672e100bcdcb34a010c4ebd488ad067014d05
johnmark <johnmarkk03@gmail.com>
2025-02-05 15:53:44 +0800

    day3 with window num

--- src/routes/apo-services.svelte
+++ src/routes/apo-services.svelte
@@ -5,9 +5,17 @@
     let modalVisible = false;
     let queueNumber = 0;
 
+    let windowNum = 0;
+
     function addQueue() {
         queueNumber++; // Increment the queue number each time the modal is triggered
         modalVisible = true;
+    }
+
+    function changeWindowNum(newNum) {
+        if (typeof newNum === "number") {
+            windowNum = newNum;
+        }
     }
 
     const dispatch = createEventDispatcher();
@@ -34,6 +42,16 @@
     function closeFidelityModal() {
         isModalOpen = false;
     }
+
+    let isPriorityOpen = false;
+
+    function openPriorityModal() {
+        isPriorityOpen = true;
+    }
+
+    function closePriorityModal() {
+        isPriorityOpen = false;
+    }
 </script>
 
 <!-- Clickable background wrapper -->
@@ -58,26 +76,23 @@
             <button
                 type="button"
                 class="text-white bg-blue-600 hover:bg-blue-700 transition-transform duration-200 transform hover:shadow-lg font-bold rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2 w-100 h-40 cursor-pointer"
-                on:click={addQueue}
+                on:click={() => { openPriorityModal(); changeWindowNum(2); }}
                 >Confirmation and Certification of National Collections</button
             >
             <button
                 type="button"
                 class="text-white bg-blue-600 hover:bg-blue-700 transition-transform duration-200 transform hover:shadow-lg font-bold rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2 w-100 h-40 cursor-pointer"
-                on:click={addQueue}
-                >Opening of Bank Account</button
+                on:click={() => { openPriorityModal(); changeWindowNum(2); }}>Opening of Bank Account</button
             >
             <button
                 type="button"
                 class="text-white bg-blue-600 hover:bg-blue-700 transition-transform duration-200 transform hover:shadow-lg font-bold rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2 w-100 h-40 cursor-pointer"
-                on:click={addQueue}
-                >Re-order of MDS Check</button
+                on:click={() => { openPriorityModal(); changeWindowNum(6); }}>Re-order of MDS Check</button
             >
             <button
                 type="button"
                 class="text-white bg-blue-600 hover:bg-blue-700 transition-transform duration-200 transform hover:shadow-lg font-bold rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2 w-100 h-40 cursor-pointer"
-                on:click={addQueue}
-                >General Concerns</button
+                on:click={() => { openPriorityModal(); changeWindowNum(6); }}>General Concerns</button
             >
         </div>
     </div>
@@ -98,26 +113,22 @@
                     <button
                         type="button"
                         class="text-white bg-blue-600 hover:bg-blue-700 transition-transform duration-200 transform hover:shadow-lg font-bold rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2 w-40 h-24 cursor-pointer"
-                        on:click={addQueue}
-                        >LGU</button
+                        on:click={() => { openPriorityModal(); changeWindowNum(1); }}>LGU</button
                     >
                     <button
                         type="button"
                         class="text-white bg-blue-600 hover:bg-blue-700 transition-transform duration-200 transform hover:shadow-lg font-bold rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2 w-40 h-24 cursor-pointer"
-                        on:click={addQueue}
-                        >NGA</button
+                        on:click={() => { openPriorityModal(); changeWindowNum(3); }}>NGA</button
                     >
                     <button
                         type="button"
                         class="text-white bg-blue-600 hover:bg-blue-700 transition-transform duration-200 transform hover:shadow-lg font-bold rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2 w-40 h-24 cursor-pointer"
-                        on:click={addQueue}
-                        >SK</button
+                        on:click={() => { openPriorityModal(); changeWindowNum(4); }}>SK</button
                     >
                     <button
                         type="button"
                         class="text-white bg-blue-600 hover:bg-blue-700 transition-transform duration-200 transform hover:shadow-lg font-bold rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2 w-40 h-24 cursor-pointer"
-                        on:click={addQueue}
-                        >GOCC</button
+                        on:click={() => { openPriorityModal(); changeWindowNum(5); }}>GOCC</button
                     >
                 </div>
                 <button
@@ -131,5 +142,40 @@
     </div>
 {/if}
 
-<Modal bind:isVisible={modalVisible} {queueNumber} />
+{#if isPriorityOpen}
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
+    <div>
+        <div
+            class="absolute top-0 left-0 w-full h-screen flex items-center justify-center z-50"
+        >
+            <div class="bg-white bg-opacity-90 rounded-lg shadow-lg p-8 w-auto">
+                <div class="flex">
+                    <button
+                        type="button"
+                        class="text-white bg-red-500 hover:bg-red-600 hover:border transition-transform duration-200 transform hover:shadow-lg font-bold rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2 size-70 cursor-pointer"
+                        on:click={addQueue}
+                    >
+                        <img
+                            src="https://ph-test-11.slatic.net/p/493d128b900a54ae49002aa2fb44f8ae.jpg"
+                            alt="prio"
+                        /></button
+                    >
+                    <button
+                        type="button"
+                        class="text-white bg-blue-600 hover:bg-blue-700 transition-transform duration-200 transform hover:shadow-lg font-bold rounded-lg text-3xl px-5 py-2.5 text-center me-2 mb-2 size-70 cursor-pointer"
+                        on:click={addQueue}>No</button
+                    >
+                </div>
+                <button
+                    on:click={closePriorityModal}
+                    class="mt-4 text-white bg-red-600 hover:bg-red-700 font-bold rounded-lg py-2 px-4"
+                >
+                    Close
+                </button>
+            </div>
+        </div>
+    </div>
+{/if}
 
+<Modal bind:isVisible={modalVisible} {queueNumber} {windowNum}/>
--- src/routes/queue-number.svelte
+++ src/routes/queue-number.svelte
@@ -1,6 +1,7 @@
 <script>
     export let isVisible = false;
     export let queueNumber = 1;
+    export let windowNum = 0;
 
     function hideModal() {
         isVisible = false;
@@ -15,10 +16,10 @@
         on:click={hideModal}
     >
         <div
-            class="bg-white p-6 rounded-lg shadow-lg text-center h-[600px] flex flex-col justify-center items-center w-[700px]"
+            class="bg-white p-6 rounded-lg shadow-lg text-center h-[600px] flex flex-col justify-center items-center border w-[700px]"
             on:click|stopPropagation
         >
-            <h2 class="text-4xl font-bold">Albay Provincial Office</h2>
+            <h2 class="text-4xl font-bold mt-20">Albay Provincial Office</h2>
             <h2 class="text-xl font-bold mt-2">Queue Number</h2>
 
             <div class="flex justify-center m-5">
@@ -34,9 +35,14 @@
                 Please wait for your number to be shown on screen
             </h2>
 
+            <div>
+                <h1 class="text-6xl font-bold text-blue-600">WINDOW <span>{String(windowNum)}</span></h1>
+            </div>
+
             <button
                 class="mt-auto w-full bg-gray-800 text-white py-2 rounded-lg hover:bg-gray-900"
                 on:click={hideModal}
+                
             >
                 Close
             </button>
